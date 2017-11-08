4a7899852c6667248fa5c8b790c634a9942fe858
BelowzeroA <fitty@mail.ru>
2017-11-08 13:33:36 +0300

    initial

---
+++ starter.py
@@ -0,0 +1,10 @@
+from gensim.summarization import keywords
+from gensim.summarization import summarize
+
+
+with open('text.txt', 'r', encoding='utf-8') as text_file:
+    text = text_file.read()
+
+print('Keywords:')
+#print(keywords(text))
+print(summarize(text))
