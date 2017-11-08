ca9163227835761e8a770fae0295d850daa1cf2e
BelowzeroA <fitty@mail.ru>
2017-11-08 17:52:40 +0300

    initial

--- summarization.py
+++ summarization.py
@@ -8,15 +8,17 @@ import os
 
 class Summarizer:
 
-    def __init__(self, folder, ratio=0.2):
+    def __init__(self, folder, ratio=0.2, word_count=50):
         self.sources = []
         self.ratio = ratio
+        self.word_count = word_count
+        self.files = []
         self.load(folder)
 
     def load(self, folder):
         path = os.path.dirname(os.path.abspath(__file__)) + "\\" + folder
-        files = [f for f in listdir(path) if isfile(join(path, f))]
-        for file in files:
+        self.files = [f for f in listdir(path) if isfile(join(path, f))]
+        for file in self.files:
             filename = join(path, file)
             with open(filename, 'r', encoding='utf-8') as text_file:
                 text = text_file.read()
@@ -25,7 +27,7 @@ class Summarizer:
     def summarize_all(self):
         result = []
         for source_text in self.sources:
-            summary = summarize(source_text, word_count=50, split=True)
+            summary = summarize(source_text, word_count=self.word_count, ratio=self.ratio, split=True)
             result.append(summary)
         return result
 
